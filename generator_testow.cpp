4c24fa436964071a3d199ba1f2f6ac637716268c
rita7715 <148997788+rita7715@users.noreply.github.com>
2023-10-25 18:29:47 +0200

    Add files via upload
    Generator testów n=1000, m = 1001

---
+++ generator_testow.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ofstream out ("testy.txt");
+    int n, p, m=1001;
+    n=1000;
+    srand( (time(0)<<(time(0)%10)) + (time(0)%10) );
+    p = rand() % 1000000;
+    set <pair <int, int> >uzyte;
+    out << n << " " << p << " " << m << "\n";
+    for (int i=0;i<p;i++)
+    {
+        pair <int, int> ta;
+        ta.first = rand() % 1000 +1;
+        ta.second = rand() % 1000 + 1001;
+        if (uzyte.count(ta))
+        {
+            while (uzyte.count(ta))
+            {
+                ta.first = rand() % 1000 +1;
+                ta.second = rand() % 1000 + 1001;
+            }
+        }else
+        {
+            uzyte.insert(ta);
+            out << ta.first << " " << ta.second << "\n";
+        }
+    }
+
+
+
+
+    return 0;
+}
